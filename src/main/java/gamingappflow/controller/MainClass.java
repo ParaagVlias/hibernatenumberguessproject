c6d652308e4005598e37b8f491bb61843fc2ae4b
ParaagVilas <paraagvilas1978@gmail.com>
2025-01-16 15:49:03 +0530

    m1

---
+++ src/main/java/gamingappflow/controller/MainClass.java
@@ -0,0 +1,109 @@
+package gamingappflow.controller;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Scanner;
+
+import gamingappflow.dao.HibernateLogic;
+import gamingappflow.dao.RandomNumberGenerator;
+import gamingappflow.dto.Player;
+import gamingappflow.services.AES;
+
+public class MainClass {
+
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		HibernateLogic logic = new HibernateLogic();
+		boolean flag=true;
+		while(flag) {
+			System.out.println("1.Play the Game\n2.Fetch Object\n3.Delete Object\n4.update Object name\n5.Exit\nEnter the valid option......");
+		switch(sc.nextInt()) {
+		case 1:{
+		Player p = new Player();
+		System.out.println("Enter Name");
+		Scanner sc3=new Scanner(System.in);
+		p.setName(sc3.nextLine());
+		System.out.println("Enter Age");
+		int age = sc.nextInt();
+	    if (age <= 18) {
+	       System.out.println("You can't play");
+	       break;
+	    }else {
+	    p.setAge(age);
+		System.out.println("Enter mail id");
+		p.setEmail(sc.next());
+		System.out.println("Enter password");
+		p.setPassword(AES.encrypt(sc.next(), "77"));
+//		p.setPassword(AES.decrypt(sc.next(), "77"));
+//		p.setPassword(sc.next());
+//		System.out.println("Enter the Amount to deposite");
+//		p.setDepositedAmount(sc.nextDouble());
+		System.out.println("Enter the image path");
+		Scanner sc2 = new Scanner(System.in);
+		
+		FileInputStream stream = new FileInputStream(sc2.nextLine());
+		byte[] arr= new byte[stream.available()];
+		stream.read(arr);
+		p.setImage(arr);
+		
+		System.out.println("Enter Phone Number");
+		p.setPhNumber(sc.nextLong());
+		//---------------------------------------------
+		System.out.println("Enter the Amount to deposite");
+		double depositedAmount = sc.nextDouble();
+		p.setDepositedAmount(depositedAmount);
+		System.out.println("All the best");
+		RandomNumberGenerator generator = new RandomNumberGenerator();
+		int[] randomNumbers = generator.generator();
+//		System.out.println(Arrays.toString(randomNumbers));
+		double amountPreMatch = depositedAmount / 3;
+		double totalEarnedAmount = 0;
+		double profit = 0;
+		for(int i = 0; i<=2;i++) {
+			System.out.println("Enter number");
+			int no1 = sc.nextInt();
+			if(no1 == randomNumbers[i]) {
+				profit=profit+amountPreMatch;
+			}else {
+				depositedAmount=depositedAmount-amountPreMatch;
+				System.out.println("oops wrong guess");
+			}
+		}
+		totalEarnedAmount=profit+depositedAmount;
+		p.setProfit(profit);
+		p.setTotallearnedAmount(totalEarnedAmount);
+		
+		logic.addObject(p);
+	    }
+		}break;
+		case 2:{
+			System.out.println("Enter email to fetch the details");
+			logic.fetchObject(sc.next());
+		}break;
+		case 3: {
+			System.out.println("Enter email to delete the object");
+			logic.deleteObject(sc.next());
+		}break;
+		case 4: {
+			System.out.println("Enter email ");
+			String email = sc.next();
+			System.out.println("Enter new Name");
+			Scanner sc2 = new Scanner(System.in);
+			String newName = sc2.nextLine();
+			logic.updateObject(email, newName);
+		}break;
+		case 5: {
+			System.out.println("Thank you");
+			flag = false;
+		}
+			break;
+		default: {
+			System.out.println("Invalid option");
+		}
+		}
+		}
+	}
+
+}
